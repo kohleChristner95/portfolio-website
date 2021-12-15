70abe9e9dd34a3b7ef276b96051cff119542eb6e
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2021-12-14 21:57:59 -0500

    Added TitleHeader component.
    imported font to use for title header.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbaritem from "./Navbaritem";
+import TitleHeader from "./TitleHeader";
 
-const Navbar = () => {
+const Navbar = ({ props }) => {
   return (
-    <nav className="navbar navbar-collapse">
-      <div className="container-fluid navbar-brand">
+    <nav
+      style={{ backgroundColor: "rgb(29, 29, 102)" }}
+      className="navbar navbar-collapse"
+    >
+      <div className="container-fluid navbar-brand .navbar-toggle">
         <Navbaritem navbarSelection="Home" />
         <Navbaritem navbarSelection="Linkedin" />
+        <TitleHeader className="center-block" name="Kohle Christner" />
         <Navbaritem navbarSelection="Github" />
         <Navbaritem navbarSelection="Contact" />
       </div>
---
+++ src/Components/TitleHeader.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TitleHeader = ({ name }) => {
+  return <div className="titleHeader">{name}</div>;
+};
+
+export default TitleHeader;
