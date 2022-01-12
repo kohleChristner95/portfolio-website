894a590c4b8848d1950dd1c1f57f8a62b5b4d731
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-01-11 22:15:51 -0500

    Adding additional info on the home page.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ const App = () => {
     <div className="App">
       <Navbar />
       <div className="empty-color full-height row">
-        <div className="label col-md-4"></div>
-        <div className="label col-md-4">
+        <div className="label col-sm-4">
+          <img
+            className="img-fluid img-thumbnail"
+            alt=""
+            src="Sam-me-and-Miles.jpg"
+          />
+        </div>
+        <div className="label col-sm-4">
           <h1>About</h1>
-          <div>
+          <div style={{ lineHeight: 1.7 }}>
             Hi! Welcome to my website portfolio. My name is Kohle Christner and
             I'm a software developer based out of Shipshewana, Indiana. I
             currently help maintain several mobile apps at a RV tech company in
-            Middlebury, Indiana called Firefly Integrations. I have a huge
-            passion for learning and expanding my programming skills. I have
-            experience using javascript with ionic capacitor and deploying new
-            app releases to the android and iOS stores. Please take a look at my
-            github account to see my latest projects or contact me for
-            inquiries!
-            <h2>Personal interests</h2>
-            <div>
+            Middlebury, Indiana called{" "}
+            <a href="https://fireflyint.com/">Firefly Integrations</a>. Being
+            self taught, I have a huge passion for learning and expanding my
+            programming skills. I have experience using javascript with ionic
+            capacitor as well as devops experience deploying new app releases to
+            the android and iOS stores. Please take a look at my github account
+            to see my latest projects or contact me for inquiries!
+            <h2>Personal Interests</h2>
+            <div style={{ lineHeight: 1.7 }}>
               I have a beautiful wife Samantha and a 6 month old son Miles. We
               love to do life together. It's not abnormal for us to get home
               from work and just chill on the floor while we have something
@@ -29,8 +36,11 @@ const App = () => {
             </div>
           </div>
         </div>
-        <div className="label col-md-4"></div>
+        <div className="label col-sm-4">
+          <img className="img-fluid img-thumbnail" alt="" src="Sam-and-I.jpg" />
+        </div>
       </div>
+      <footer>Made by Kohle Christner 🔥</footer>
     </div>
   );
 };
