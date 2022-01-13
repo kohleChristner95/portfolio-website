befd256defc5265a9336c8e0e31df38e3bd968e9
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-01-12 21:45:22 -0500

    Started creating form component.

--- src/App.js
+++ src/App.js
@@ -20,19 +20,22 @@ const App = () => {
             I'm a software developer based out of Shipshewana, Indiana. I
             currently help maintain several mobile apps at a RV tech company in
             Middlebury, Indiana called{" "}
-            <a href="https://fireflyint.com/">Firefly Integrations</a>. Being
-            self taught, I have a huge passion for learning and expanding my
-            programming skills. I have experience using javascript with ionic
-            capacitor as well as devops experience deploying new app releases to
-            the android and iOS stores. Please take a look at my github account
-            to see my latest projects or contact me for inquiries!
+            <a target="_blank" rel="noreferrer" href="https://fireflyint.com/">
+              Firefly Integrations
+            </a>
+            . Being self taught, I have a huge passion for learning and
+            expanding my programming skills. I have experience using javascript
+            with ionic capacitor as well as devops experience deploying new app
+            releases to the android and iOS stores. Please take a look at my
+            github account to see my latest projects or contact me for
+            inquiries!
             <h2>Personal Interests</h2>
             <div style={{ lineHeight: 1.7 }}>
               I have a beautiful wife Samantha and a 6 month old son Miles. We
               love to do life together. It's not abnormal for us to get home
               from work and just chill on the floor while we have something
               playing on tv. Other personal interests I have are hunting, video
-              games, and I'm a big music enthusiast.
+              games, and I love listening to music.
             </div>
           </div>
         </div>
