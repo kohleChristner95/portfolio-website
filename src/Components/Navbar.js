838490bf312230c5558ff11e5e038f00173ee2d5
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-01-15 23:00:38 -0500

    Navbar funtional!

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,15 +10,15 @@ const Navbar = (props) => {
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav">
           <li className="nav-item">
             <a className="nav-link" href="/Home">
