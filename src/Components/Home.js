b40720a2bf5f3d88c5104da514087a7cd9da14b4
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-02-05 22:41:20 -0500

    Updated home page info. Also incorporated new paragraph-text class to add margin on the right and left to give paragraph text a little cleaner look.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
         />
       </div>
       <div className="label">
-        <div className="my-4" style={{ lineHeight: 1.7 }}>
+        <div className="my-4 paragraph-text" style={{ lineHeight: 1.7 }}>
           Hi! Welcome to my website portfolio. My name is Kohle Christner and
           I'm a software developer based out of Shipshewana, Indiana. I
           currently help maintain several mobile apps at a RV tech company in
@@ -25,13 +25,14 @@ const Home = () => {
           capacitor as well as devops experience deploying new app releases to
           the android and iOS stores. Please take a look at my github account to
           see my latest projects or contact me for inquiries!
-          <h2>Personal Interests</h2>
+          <div>&nbsp;</div>
+          <h2>About</h2>
           <div style={{ lineHeight: 1.7 }}>
             I have a beautiful wife Samantha and a 6 month old son Miles. We
             love to do life together. It's not abnormal for us to get home from
             work and just chill on the floor while we have something playing on
-            tv. Other personal interests I have are hunting, video games, and I
-            love listening to music.
+            tv. Other personal interests I have are hunting, video games, and
+            traveling with my family.
           </div>
         </div>
       </div>
